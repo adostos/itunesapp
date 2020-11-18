934bd2e4e7e6d1bdb51aee63cfac0033b601f5f9
adostos <ado19960@gmail.com>
2020-11-18 15:43:56 +0100

    dist - obr

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
 	<section class="home">
-		<img alt="Vue logo" src="../assets/logo.png">
+		<img
+			alt="Vue logo"
+			src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.stickpng.com%2Fassets%2Fimages%2F585bbb3acb11b227491c32b0.png&f=1&nofb=1"
+		/>
 		<the-home />
 	</section>
 </template>
@@ -18,5 +21,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+img {
+	width: 25%;
+	margin: 4em 0 3em 0;
+}
 </style>
